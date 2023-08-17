fae908db0a247dbcac3291f62629eea84558cdd6
vattamnitish <vattamnitish8@gmail.com>
2023-08-17 11:07:05 +0530

    hello world script added

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "Hello world,i am learning devops"
+
+PERSON1=Sachin
+PERSON2=Rohit
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
