71112cf8826eaefb9410789b7c46c43cf3d1a420
vattamnitish <vattamnitish8@gmail.com>
2023-08-17 11:32:19 +0530

    hello world script added

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
