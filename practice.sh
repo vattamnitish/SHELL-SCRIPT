ce83396c9320e38b1f34a0bd9a6a712303c302e8
vattamnitish <vattamnitish8@gmail.com>
2023-08-19 20:09:03 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-PERSONS=("ramesh" "suresh" "bhavana")
-echo "first person: ${PERSONS[0]}"
-echo "all persons: ${PERSONS[@]}"
+NUMBER = $1
+if [ $NUMBER -gt 10 ]
+then
+    echo "$NUMBER is greater than 10"
+else
+    echo "$NUMBER is less than 10"
+fi
 
 
