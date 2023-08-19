68ee048239c9d4c3c0eed76f723893696bbd33a4
vattamnitish <vattamnitish8@gmail.com>
2023-08-19 18:54:34 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "ramesh: Hi suresh good morning"
-echo "suresh: good morning ramesh"
-echo "ramesh: how are you suresh"
-echo "suresh: i am good ramesh"
+PERSON1=ramesh
+PERSON2=suresh
+echo "$PERSON1: Hi $PERSON2 good morning"
+echo "$PERSON2: good morning $PERSON1"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: i am good $PERSON1"
