48c5d723120ba62d1384f6cd5cbd47ec398cc96a
vattamnitish <vattamnitish8@gmail.com>
2023-08-21 10:23:13 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-USERID=$(id-u)
-if [ $USERID -ne 0 ]
-then
-    echo "error:please run the script in sudo access"
-    exit 1
-fi
-yum install mysql -y
 
 USERID=$(id-u)
-if [ $USERID ne 0]
+if [ $USERID ne 0 ]
 
     then
     echo "please run the scripr in root access"
