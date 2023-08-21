71aeddb8b1be4abaebdf3c43798f7766c034b09c
vattamnitish <vattamnitish8@gmail.com>
2023-08-21 13:28:25 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -9,6 +9,20 @@ if [ $USERID -ne 0 ]
 fi
 
 yum install mysql -y
+if [ $? -ne 0]
+    then
+    echo "installation of my sql is error"
+    else
+    echo "installation of my sql is sucess"
+fi
+yum install postfix -y
+if [ $? -ne 0 ]
+    then
+    echo "installation of post fix is error"
+    else
+    echo "installation of postfix is sucess"
+fi
+
 
 
 
