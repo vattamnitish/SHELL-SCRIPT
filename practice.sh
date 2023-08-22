c7f9968f4d7e975282dfee7eed3170b0e8187b27
vattamnitish <vattamnitish8@gmail.com>
2023-08-22 11:28:57 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,38 +1,59 @@
 #!/bin/bash
+#!/bin/bash
 
 DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
-
-VALIDATE (){
-    if [ $1 -ne 0 ]
-    then
-    echo -e "$2... $R is failure $N"
-    exit 1
-    else
-    echo -e "$2.... $G is sucess $N"
-    fi
-
-}
-
-
-USERID=$(id -u)
-if [ $USERID -ne 0 ]
-
-    then
-    echo "please run the scripr in root access"
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
 
-yum install mysql -y &>>$LOGFILE
-VALIDATE $? "INSTALLING my sql"
-   
-yum install postfix -y &>>$LOGFILE
-VALIDATE $? "INSTALLING post fix"
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copied MongoDB repo into yum.repos.d"
+
+yum install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installation of MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Edited MongoDB conf"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting MonogoDB"
+
+
 
 
 
